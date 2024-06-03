71521d9cb410c177d5b1d815d500592c36b57d24
Ahnheechan <ahc70032@gmail.com>
2024-06-03 21:42:03 +0900

    [feat]댓글 삭제 기능 추가

--- src/main/java/com/sejong/recycle/comment/dto/CommentResDto.java
+++ src/main/java/com/sejong/recycle/comment/dto/CommentResDto.java
@@ -8,10 +8,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CommentResDto {
+
+    private Long commentId;
     private String content;
     private String writer;
 
     public CommentResDto(Comment comment) {
+        this.commentId = comment.getId();
         this.content = comment.getContent();
         this.writer = comment.getWriter();
     }
